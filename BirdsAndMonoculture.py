91250701a5c6cd7219fb6b3ea5a7e772e76ae7ca
Boyd Hamilton <boydroberthamilton@gmail.com>
2024-01-11 13:26:59 -0700

    birds + monoculture first finish

--- BirdsAndMonoculture.py
+++ BirdsAndMonoculture.py
@@ -5,23 +5,40 @@ import numpy as np
 import pandas as pd
 
 filename = "data/41467_2016_BFncomms12717_MOESM1715_ESM.csv"
+
+file_opened = open(filename, 'r')
+ 
+# creating dictreader object
+file = csv.DictReader(filename)
+
+df=[]
 rows = []
 
-with open(filename, 'r') as csvfile:
+columns = {}
+column_count = {}
     
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        rows.append(row)
+with open(filename, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    # Assuming the first row contains column headers
+    for column in csv_reader.fieldnames:
+        columns[column] = []
+    
+    # Iterate through each row and append values to respective columns
+    for row in csv_reader:
+        for column in csv_reader.fieldnames:
+            columns[column].append(row[column])
 
-        
-print(rows)
-tree_species=[]
+for key in columns:
+    if(key!="Species"):
+        column_count[key]=(columns[key].count("Y"))
 
+print(column_count)
 
-print(tree_species)
+keys = list(column_count.keys())
+values = list(column_count.values())
 
-# plt.barh(y = location_culled, width = amount, color ='maroon', 
-#          height = 0.4)
+plt.barh(keys, values)
 
 plt.xlabel("Amount of Given Species in Given Location")
 plt.ylabel("Location + Species")

dd8eb91c504758e099c3837dee5b48a67abfb4da
Boyd Hamilton <boydroberthamilton@gmail.com>
2024-01-11 14:24:11 -0700

    culling data + trendline + rscore for birds and monoculture

--- BirdsAndMonoculture.py
+++ BirdsAndMonoculture.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 filename = "data/41467_2016_BFncomms12717_MOESM1715_ESM.csv"
 
@@ -30,17 +31,31 @@ with open(filename, 'r') as csv_file:
             columns[column].append(row[column])
 
 for key in columns:
-    if(key!="Species"):
+    if(key!="Species"and ("Cropland") not in key):
         column_count[key]=(columns[key].count("Y"))
-
-print(column_count)
 
 keys = list(column_count.keys())
 values = list(column_count.values())
 
-plt.barh(keys, values)
+combo = list(zip(keys,values))
+ranked_data = sorted(combo, key=lambda x: x[1], reverse=True)
+ranked_keys = [item[0] for item in ranked_data]
+ranked_values = [item[1] for item in ranked_data]
 
-plt.xlabel("Amount of Given Species in Given Location")
-plt.ylabel("Location + Species")
-plt.title("Canadian Trees")
+x_values=[0,1,2,3,4]
+
+z = np.polyfit(x_values, ranked_values, 1)
+p = np.poly1d(z)
+
+coefficient_of_dermination = r2_score(ranked_values, p(x_values))
+
+print(coefficient_of_dermination)
+
+plt.plot(x_values, p(x_values),color="red") # trendline
+
+plt.bar(ranked_keys, ranked_values)
+
+plt.xlabel("Tree Species")
+plt.ylabel("Birds in Biome")
+plt.title("Birds in Tree Biome")
 plt.show()
